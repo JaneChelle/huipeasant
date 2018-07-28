3b355f6f6e143f37f1252538e93850bf3f4f4b7b
wky181 <1813721268@qq.com>
2018-07-28 09:42:41 +0800

    删除目录target

--- target/classes/static/js/train.js
+++
@@ -1,9 +0,0 @@
-//二维码
-$(document).ready(function(){
-  $("#qr-code").mouseenter(function(){
-    $(".qr-code").fadeIn();
-  });
-  $(".qr-code").mouseleave(function(){
-    $(".qr-code").fadeOut();
-  });
-});
