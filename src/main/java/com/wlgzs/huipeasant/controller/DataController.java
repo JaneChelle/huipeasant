d729b72cf04fe9f5355502b948f43d6913d1d48d
wky181 <1813721268@qq.com>
2018-07-31 22:06:16 +0800

    ddddddd

--- src/main/java/com/wlgzs/huipeasant/controller/DataController.java
+++ src/main/java/com/wlgzs/huipeasant/controller/DataController.java
@@ -1,11 +1,13 @@
 package com.wlgzs.huipeasant.controller;
 
 import com.wlgzs.huipeasant.base.BaseController;
+import com.wlgzs.huipeasant.dao.UserRepository;
 import com.wlgzs.huipeasant.entity.Data;
 import com.wlgzs.huipeasant.entity.User;
 import com.wlgzs.huipeasant.entity.Video;
 import com.wlgzs.huipeasant.util.Result;
 import com.wlgzs.huipeasant.util.ResultCode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,6 +19,8 @@ import java.util.List;
 @RequestMapping("/user")
 @RestController
 public class DataController extends BaseController {
+     @Autowired
+    UserRepository userDao;
 
     @RequestMapping("toindex")       //   进入pc端主页
     public ModelAndView toindex(Model model) {
@@ -100,22 +104,23 @@ public class DataController extends BaseController {
 
 
     @PostMapping("addData")         //  添加数据
-    public Result addData(Data data, MultipartFile multipartFile, int dataLevle) throws IOException {
+    public ModelAndView addData(Data data, MultipartFile multipartFile, int dataLevle) throws IOException {
         System.out.println("tgdfygdryr"+dataLevle);
 
         boolean isTrue = dataService.addData(data, multipartFile, dataLevle);
-        if (isTrue) {
-            return new Result(ResultCode.SUCCESS, "上传成功");
+        if (dataLevle==2) {
+            return new ModelAndView("redirect:/user/toaddData");
         } else {
-            return new Result(ResultCode.FAIL, "上传失败，请检查信息是否填写完整");
+            return new ModelAndView("");
         }
     }
 
 
     @GetMapping("textview/{dataId}")   //进入文章页面
     public ModelAndView textView(Model model, @PathVariable("dataId") long dataId) {
-
-        model.addAttribute("data", dataService.dataView(dataId));
+        Data data =  dataService.dataView(dataId);
+        model.addAttribute("data",data);
+        model.addAttribute("user",userDao.findById(data.getUserId()));
 
         if (dataService.jundegeView(dataId)) {
             model.addAttribute("paragraphs", dataService.paragraphList(dataService.textView(dataId).getContents()));
@@ -126,6 +131,7 @@ public class DataController extends BaseController {
             return new ModelAndView("article");
         } else {
             model.addAttribute("answer", commentService.getanswer(dataId));
+
             return new ModelAndView("ask");
         }
     }
